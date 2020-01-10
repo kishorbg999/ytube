aa110cb8bf26b57cefb6c778a8bd25825e0af38c
yo62982n <yo62982n@pace.edu>
2020-01-11 08:45:00 +0900

    modify sql server and add Procfile

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,8 @@ session_start();
 date_default_timezone_set("America/New_York");
 
 try {
-    $con = new PDO("mysql:dbname=youtube;host=localhost", "root", "");
+    $con = new PDO("mysql:dbname=OF5seLKKxG;host=remotemysql.com;port=3306", "OF5seLKKxG", "fhG7JEtc9Z");
+    // $con = new PDO("mysql:dbname=youtube;host=localhost", "root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch(PDOException $e){
